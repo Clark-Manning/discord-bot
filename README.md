dad4daecd0cc0c5729b79f35072bd4d6d489a8b5
Clark-Manning <clakeyman@gmail.com>
2023-05-19 15:58:25 -0700

    Add toggalable edit and delete commands to display edited and deleted msgs

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Personal discord bot made for me to add any fun functionality to as I please.
 
 ## Setup
 
-Prior to running, create a `.env` file and add your discord token under the `TOKEN` variable and your Spoontacular api key under the `SPOONACULAR_API_KEY` variable.
+Prior to running, create a `.env` file and add your discord token under the `TOKEN` variable and your Spoontacular api key under the `SPOONACULAR_API_KEY` variable. You may also set `SHOW_EDITS` and `SHOW_DELETES` in the `.env`, though they will default to `True` and can be set while the bot is running in Discord as well.
 
 Next run `pip install -r`
 
@@ -23,3 +23,25 @@ Use this command to ready up, or unready if you are already ready. The bot will 
 ### !recipe
 
 Use this command to display a random recipe. The bot will respond with embedded text displaying a recipe, a link to the recipe, and an image. You may refine the randomness by adding arguments to your command like so `!recipe mediterranean` or `!recipe vegetarian dessert`. Uses the Spoontacular random recipe api
+
+### !delete
+
+Toggles `show_deletes` to True or False respectively
+
+### !edit
+
+Toggles `show_edits` to True or False respectively
+
+## Background Processes
+
+### On Message Delete
+
+If `show_deletes=True` (which is the default, but can be set to `False` in `.env`) then when a message is deleted, the bot sends an embedded message saying the name of the person who deleted the message, the message, and the time of deletion
+
+`show_deletes` can also be toggled in discord by using the `!delete` command
+
+### On Message Edit
+
+If `show_edits=True` (which is the default, but can be set to `False` in `.env`) then when a message is edited, the bot sends an embedded message which the editors name, the before message, and the editted message.
+
+`show_edits` can also be toggled in Discord by using the `!edit` command
--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 import discord
 from discord.ext import commands
 import os
-import requests
 from dotenv import load_dotenv
 import spoonacular as sp
+import time
 
 load_dotenv()
 
@@ -14,6 +14,8 @@ SPOONACULAR_API_KEY = os.getenv('SPOONACULAR_API_KEY')
 spoon = sp.API(SPOONACULAR_API_KEY)
 
 ready_members = {}
+show_edits = os.getenv('SHOW_EDITS', True)
+show_deletes = os.getenv('SHOW_DELETES', True)
 
 intents = discord.Intents.default()
 intents.members = True
@@ -30,7 +32,9 @@ async def on_ready():
         for member in guild.members:
             if not member.name == 'playground-bot':
                 ready_members[member.name + '#' + member.discriminator] = False
+    
     print(f'Guild Members: {ready_members}')
+    print(f'Show edits: {show_edits}\nShow deletes: {show_deletes}')
 
 
 @bot.command()
@@ -47,6 +51,7 @@ async def ready(ctx):
 
     if all(value == True for value in ready_members.values()):
         await ctx.send('All members ready\nThe pact may proceed.')
+
 
 @bot.command()
 async def recipe(ctx, *args):
@@ -67,6 +72,46 @@ async def recipe(ctx, *args):
         await ctx.send(embed=embed)
     except:
         await ctx.send('Failed to get recipe, try using a different keyword, or try again later')
+
+@bot.command()
+async def edit(ctx):
+    global show_edits
+    show_edits = not show_edits
+    if show_edits:
+        await ctx.send('I am currently displaying edits')
+    else:
+        await ctx.send('I am no longer displaying edits')
+
+
+@bot.command()
+async def delete(ctx):
+    global show_deletes
+    show_deletes = not show_deletes
+    if show_deletes:
+        await ctx.send('I am currently displaying deleted messages')
+    else:
+        await ctx.send('I am no longer displaying deleted messages')
+
+
+@bot.event
+async def on_message_delete(message):
+    global show_deletes
+    if show_deletes:
+        t = time.localtime()
+        current_time = current_time = time.strftime("%H:%M:%S", t)
+        embed = discord.Embed()
+        embed.add_field(name=f'{message.author} deleted the following message at {current_time}:', value=message.content)
+        await message.channel.send(embed=embed)
+
+
+@bot.event
+async def on_message_edit(message_before, message_after):
+    global show_edits
+    if show_edits:         
+        embed = discord.Embed(title=f'{message_before.author} made an edit!')
+        embed.add_field(name=f'Before:', value=message_before.content, inline=False)
+        embed.add_field(name=f'Edited:', value=message_after.content, inline=False)
+        await message_before.channel.send(embed=embed)
 
 
 @bot.command()
